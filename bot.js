879ec9baeef52889e7000972a758bed0fc43b182
Vincent Mahnke <accounts@vimaster.de>
2017-10-09 01:54:21 +0200

    ~ Polished log messages - User- + text-channel-ID exposed to environment variables

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const username = "Rette";
-const discriminator = "7921";
-const musicTextChannel = "340113556601044993";
 
 client.on('ready', () => {
   console.log('Discord library is ready!');
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Waiting for user ${process.env.DISCORD_USERNAME}#${process.env.DISCORD_DISCRIMINATOR}!`);
 });
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+  	console.log(`voiceStateUpdate on user ${newMember.user.username}#${newMember.user.discriminator}`);
+
 	if (typeof oldMember.voiceChannelID == "string")
 	{
 		return;
@@ -20,29 +20,32 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
 		return;
 	}
 
-	if (newMember.user.username != username)
+	if (newMember.user.username != process.env.DISCORD_USERNAME)
 	{
 		return;
 	}
 
-	if (newMember.user.discriminator != discriminator)
+	if (newMember.user.discriminator != process.env.DISCORD_DISCRIMINATOR)
 	{
 		return;
 	}
+
+  	console.log(`	We care about this user; joining same voice channel '${newMember.voiceChannel.name}'`);
 
 	newMember.voiceChannel.join()
   		.then(connection => {
-  			console.log("waiting after join...");
+  			console.log(`	Joined - quick pause...`);
   			var waitTill = new Date(new Date().getTime() + 1000);
 			while(waitTill > new Date()){}
-  			console.log("sending command...");
-  			client.channels.find("id", musicTextChannel).send('!play https://www.youtube.com/watch?v=UhLJ0sFBS40')
-  			.then(connection => {
-	  			var waitTill = new Date(new Date().getTime() + 2000);
-				while(waitTill > new Date()){}
-	  			console.log("leaving...");
-				newMember.voiceChannel.leave()
-  			});
+  			console.log(`	Requesting song...`);
+  			client.channels.find("id", process.env.DISCORD_MUSICTEXTCHANNELID).send('!play https://www.youtube.com/watch?v=UhLJ0sFBS40')
+	  			.then(connection => {
+  					console.log(`	Song requested`);
+		  			var waitTill = new Date(new Date().getTime() + 2000);
+					while(waitTill > new Date()){}
+					console.log(`	Leaving room again...`);
+					newMember.voiceChannel.leave()
+	  			});
 		})
  		.catch(console.error);
 });
